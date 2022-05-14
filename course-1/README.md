13037ca93140df79e181dbe7a521a1b7caeb7269
sameer05515 <samee.geeee@gmail.com>
2022-05-14 18:11:32 +0530

    commit

---
+++ course-1/README.md
@@ -0,0 +1,23 @@
+# to run this code
+node app.js
+
+# to start with NPM
+npm init
+
+# To start server
+npm run start-server
+
+or 
+
+npm start
+
+# nodemon development dependency installation
+1. npm install nodemon --save-dev
+2.
+
+# Types of errors
+    
+1. Syntax errors
+2. Runtime Errors
+    1. Logical error
+    2. Using Nodejs debugger
---
+++ practice-1/app.js
@@ -0,0 +1,44 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    route = req.url;
+    method= req.method;
+    res.setHeader('Content-Type','text/html')
+    if (route === '/' && method==='GET') {        
+        res.write(`<form action="/greet" method="post">
+        <input type="text" name="message">
+        <button type="submit">Submit</button>
+    </form>`);
+        return res.end();
+    } else if (route === '/users' && method==='GET') {
+        res.write(`<ul>
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+        <li>User 4</li>
+        <li>User 5</li>
+        </ul>`);
+        return res.end();
+    }else if(route === '/greet' && method==='POST' ){
+        const body = [];
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        })
+
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString().split('=')[1];
+            console.log(parsedBody);
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            return res.end();
+        })
+
+    }else{
+        res.write(`Page not found : <b>${route}</b> !!`);
+        return res.end();
+    }
+
+
+})
+
+server.listen(3000);
